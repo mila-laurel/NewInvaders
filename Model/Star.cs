ee566de8351508180f55c7a0a11e9e9baafcda8d
Мила Лаврова <mila.fourteen@gmail.com>
2020-08-20 17:34:29 +0300

    Add project files.

---
+++ Model/Star.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace NewInvaders.Model
+{
+    class Star
+    {
+        public Point Location { get; private set; }
+        public Color Pen { get; set; }
+        public UIElement StarControl { get; set; }
+        public Star(Point point, Color pen, UIElement starControl)
+        {
+            Location = point;
+            Pen = pen;
+            StarControl = starControl;
+        }
+    }
+}
---
+++ ViewModel/Animation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using NewInvaders.View;
+using NewInvaders.Model;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace NewInvaders.ViewModel
+{
+    class Animation
+    {
+        private readonly ObservableCollection<UIElement>_sprites = new ObservableCollection<UIElement>();
+        private readonly Dictionary<Star, SimpleStar> _simleStars = new Dictionary<Star, SimpleStar>();
+        private readonly Dictionary<Star, AnotherKindOfStar> _otherStars = new Dictionary<Star, AnotherKindOfStar>();
+        private readonly Dictionary<Star, TheLastKindOfStar> _lastStars = new Dictionary<Star, TheLastKindOfStar>();
+        private DispatcherTimer _timer = new DispatcherTimer();
+
+        void timer_Tick(object sender, object e)
+        {
+            
+        }
+    }
+}
